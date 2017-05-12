5aeb2663d89a969f373fa0d068cea7acfe4ead53
Joe Kozlowski <kozineffect@github.com>
2017-05-11 21:37:41 -0400

    Added code for basic functionality

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -16,33 +16,43 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    else
-    {
+    else {
         console.log("success");
-                connection.query("SELECT * FROM products", function (err, results) {
+        connection.query("SELECT * FROM products", function (err, results) {
             if (err) throw err;
             else {
                 for (var i = 0; i < results.length; i++) {
-                        console.log("Product id: " + results[i].item_id);
-                        console.log("Product name: " + results[i].product_name);
-                        console.log("Department: " + results[i].department_name);
-                        console.log("Current price: " + results[i].price);
-                        console.log("Current quantity: " + results[i].stock_quantity);
-                        console.log("---------------------");
+                    console.log("Product id: " + results[i].item_id);
+                    console.log("Product name: " + results[i].product_name);
+                    console.log("Department: " + results[i].department_name);
+                    console.log("Current price: " + results[i].price);
+                    console.log("Current quantity: " + results[i].stock_quantity);
+                    console.log("---------------------");
                 }
                 inquirer.prompt({
                     name: "product_id",
                     type: "input",
                     message: "What is the id # of the product you want to buy?"
-                }).then(function(answer){
+                }).then(function (answer) {
                     var product_id = answer.product_id;
                     inquirer.prompt({
                         name: "quantity",
                         type: "input",
                         message: "How many of the item do you care to buy?"
-                    }).then(function(answer){
-                        console.log(product_id);
-                        console.log(answer.quantity);
+                    }).then(function (answer) {
+                        connection.query("SELECT stock_quantity FROM products WHERE item_id = " + product_id, function (err, results) {
+                            if (err) throw err;
+                            var new_quantity = results[0].stock_quantity - answer.quantity;
+                            new_quantity = parseInt(new_quantity);
+                            if (results[0].stock_quantity >= answer.quantity) {
+                                console.log("Your order has been accepted!");
+                                connection.query("UPDATE products SET stock_quantity = " + new_quantity + " WHERE item_id = " + product_id, function (err, results) {
+                                    if (err) throw err;
+                                });
+                            } else {
+                                console.log("Insufficient quantity in store!");
+                            }
+                        });
                     });
                 });
             }
