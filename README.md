8bd1d4c0091f9e596f78a50ca45a6547984c6dc6
Joe Kozlowski <kozineffect@github.com>
2017-05-12 23:33:09 -0400

    Added comments and link to video proof in markdown file

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # BAMAZON
 Bamazon MySQL storefront
+[Link to video proof](https://youtu.be/at6wdYcBfbA)
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -14,6 +14,7 @@ var connection = mysql.createConnection({
     database: "BAMAZON"
 });
 
+//Retrieve data from database
 connection.connect(function (err) {
     if (err) throw err;
     else {
@@ -29,17 +30,19 @@ connection.connect(function (err) {
                     console.log("Current quantity: " + results[i].stock_quantity);
                     console.log("---------------------");
                 }
+
+                //ask the customer what they want to purchase
                 inquirer.prompt({
                     name: "product_id",
                     type: "input",
                     message: "What is the id # of the product you want to buy?"
-                }).then(function (answer) {
+                }).then(function (answer) { //ask customer how much they care to purchase
                     var product_id = answer.product_id;
                     inquirer.prompt({
                         name: "quantity",
                         type: "input",
                         message: "How many of the item do you care to buy?"
-                    }).then(function (answer) {
+                    }).then(function (answer) { //make sure there isenough of the item in stock and change the inventory
                         connection.query("SELECT stock_quantity FROM products WHERE item_id = " + product_id, function (err, results) {
                             if (err) throw err;
                             var new_quantity = results[0].stock_quantity - answer.quantity;
